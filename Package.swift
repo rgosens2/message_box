d36c4f7c275cbb178cc9bd16f82749c4d0e0075b
rgosens2 <rgosens2@gmail.com>
2025-10-16 10:56:07 +0200

    package

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.7.2
+import PackageDescription
+
+let package = Package(
+  name: "MyBoxes",
+  targets: [ .executableTarget( 
+    name: "mb5", 
+    path: ".", // root folder 
+    exclude: ["Package.swift"], // exclude itself 
+    sources: ["."], // include all Swift files automatically 
+    publicHeadersPath: nil ) 
+  ] 
+)
